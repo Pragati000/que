35b847ba1bd4450ddb8c2aedbe106fceb66311fe
Pragati000 <pragati.goswami_ccv172gla.ac.in>
2019-01-28 04:21:12 -0800

    this is second commit  commit

--- main/MyQueue.java
+++ main/MyQueue.java
@@ -44,37 +44,42 @@ public class MyQueue {
         else
             return false;
         }
-        void  dequeu(){
+
+        int dequeu(){
         if(!isEmpty()){
           int ele=queu[front];
+          front++;
           for(int i=0;i<size-1;i++)
               queu[i]=queu[i+1];
-               rear--;
+          queu[rear]=0;
+          return ele;
         }
         else
             System.out.println("que is empty");
-
+return 0;
         }
 
         public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("enter element in que");
         MyQueue q1=new MyQueue();
+            System.out.println("enque operation");
         for(int i=0;i<10;i++)
         {
             int a=sc.nextInt();
             q1.enqueu(a);
-        }
+        }//enque operation call
             for(int i=0;i<10;i++)
             {
-                System.out.println(q1.queu[i]+"\n");
-            }
+                System.out.print(q1.queu[i]+"\n");
+            }//display after enqueu
 
 
-            q1.dequeu();
+            System.out.println("display after removing element"+" "+q1.dequeu());
+
             for(int i=0;i<10;i++)
             {
-                System.out.println(q1.queu[i]+"\n");
+                System.out.print(q1.queu[i]+"\n");
             }
         }
 
